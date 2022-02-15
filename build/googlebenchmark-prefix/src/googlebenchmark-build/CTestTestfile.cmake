11110cdcbea2302784f13bfaa2b0db6f09512127
horsada <ah2719@ic.ac.uk>
2022-02-15 12:21:43 +0000

    Remove build folder

--- build/googlebenchmark-prefix/src/googlebenchmark-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ah2719/Advanced_Databases/Coursework1/Query_Processing_ah2719/build/googlebenchmark-prefix/src/googlebenchmark
-# Build directory: /home/ah2719/Advanced_Databases/Coursework1/Query_Processing_ah2719/build/googlebenchmark-prefix/src/googlebenchmark-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
